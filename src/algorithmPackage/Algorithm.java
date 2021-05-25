b209bf9efd93b97e5b5ff80f52978d1812bc5530
gcfbn <bmekarski@interia.pl>
2021-05-25 19:54:27 +0200

    Added common strategy method to interface Algorithm (missing file in commit)

--- src/algorithmPackage/Algorithm.java
+++ src/algorithmPackage/Algorithm.java
@@ -2,11 +2,14 @@ package algorithmPackage;
 
 import resultsPackage.Results;
 import systemPackage.CPU;
+import systemPackage.Process;
 import systemPackage.ProcessSender;
 
 import java.util.ArrayList;
+import java.util.Queue;
 
 public interface Algorithm {
 
     Results executeAlgorithm(ArrayList<CPU> cpus, ArrayList<ProcessSender> processes, int maxTime);
+    boolean tryToExecuteProcess(Process currentProcess, ArrayList<CPU> cpus, Queue<Process> queue);
 }
